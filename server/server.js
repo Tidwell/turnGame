b21bcf9019f8f1c64fb4cecb754978b4151be9e1
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2011-03-24 03:15:30 +0000

    Fixed so serveHttp is respected

--- server/server.js
+++ server/server.js
@@ -14,8 +14,7 @@ generalGameServer.prototype.createServer = function(obj) {
   //assignments/loading
   var 
       //node.js libraries
-      http = require('http')
-    , fs = require('fs')
+      fs = require('fs')
     , sys = require('sys')
     , server
     //module libraries
@@ -23,8 +22,7 @@ generalGameServer.prototype.createServer = function(obj) {
     , log = require('logging');
 
   //custom objects  
-  var httpUtil = require('./util/httpUtil.js')
-    , socketUtil = require('./util/socketUtil.js')
+  var socketUtil = require('./util/socketUtil.js')
 
   //path to where all the front-end code lives (html/css/js)
   var clientFolderPath = obj.server.clientDir ? obj.server.clientDir : 'client';
@@ -39,31 +37,39 @@ generalGameServer.prototype.createServer = function(obj) {
   
     
   //todo accept serveHttp arg from passed in object and convert to use simple http (maybe with jqtpl)
-  //create the http server
-  var server = http.createServer(function(req, res){
-    //when we get a HTTP request
-    //get the requested system path
-    var systemPath = httpUtil.getSystemPathFromRequest({
-      clientFolderPath: clientFolderPath, 
-      req: req
-    });
-    //try to grab the file
-    fs.readFile(systemPath, function(err, data){
-      //if the file cannot be found
-      if (err) {
-        //send the client a 404 response
-        httpUtil.send404(res);
+  
+  if (obj.server.serveHttp) {
+    var http = require('http');
+    var httpUtil = require('./util/httpUtil.js');
+    //create the http server
+    var server = http.createServer(function(req, res){
+      //when we get a HTTP request
+      //get the requested system path
+      var systemPath = httpUtil.getSystemPathFromRequest({
+        clientFolderPath: clientFolderPath, 
+        req: req
+      });
+      //try to grab the file
+      fs.readFile(systemPath, function(err, data){
+        //if the file cannot be found
+        if (err) {
+          //send the client a 404 response
+          httpUtil.send404(res);
+          return;
+        }
+        //else, we can send back the file that maps to the requested url    
+        httpUtil.sendFile({res: res, data: data, path: systemPath});
         return;
-      }
-      //else, we can send back the file that maps to the requested url    
-      httpUtil.sendFile({res: res, data: data, path: systemPath});
-      return;
+      });
     });
-  });
 
-  //listen
-  server.listen(8080);
- 
+    //listen
+    server.listen(8080);
+  }
+  else {
+    var server = obj.server.server;
+  }
+  
   //set up socket server
   var io = io.listen(server);
 
